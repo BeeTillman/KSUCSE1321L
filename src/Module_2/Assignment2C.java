ef99bb4d645221d3d88f34893e09ae856050c69e
BeeTillman <bpt3creations@gmail.com>
2022-08-27 01:49:57 -0400

    Updated Lab4C

--- src/Module_2/Assignment2C.java
+++ src/Module_2/Assignment2C.java
@@ -7,11 +7,20 @@ Instructor: Jaskirat Singh Sohal
 Name:   Billups Tillman
 Lab/Assignment#:    2
 */
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Assignment2C {
+    // Setting format for British Dollar and Bison Dollar Output
+    private static final DecimalFormat UKDBSD = new DecimalFormat("0.00");
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
+        System.out.println("[Bisonica Currency Exchange]\nGeneral Bison welcomes you to Pax Bisonica!");
+        System.out.println("How many US Dollars do you have? ");
+            int USD = sc.nextInt();
+        // Stating Conversion Constants
+        final double UKD = (USD*.85);
+        final double BSD = (UKD/5);
+            System.out.println(USD+" US Dollar(s) is "+UKDBSD.format(UKD)+" British Pound(s), which is worth "+UKDBSD.format(BSD)+" Bison Dollar(s)!");
     }
 }
--- src/Module_4/Lab4C.java
+++ src/Module_4/Lab4C.java
@@ -12,29 +12,25 @@ import java.util.Scanner;
 public class Lab4C {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
+        int inNUM = -2147483648;
         System.out.println("Welcome!\nPlease enter a number: ");
         float NUM = sc.nextFloat();
+        // While Statement ensures program doesn't end if an invalid input is received
+        while (inNUM == -2147483648) {
             System.out.println("What would you like to do with this number:\n0- Get the additive inverse of the number\n1- Get the reciprocal of the number\n2- Square the number\n3- Cube the number\n4- Exit the program");
-            int inNUM = sc.nextInt();
-                if ((inNUM ==0)||(inNUM ==1)||(inNUM ==2)||(inNUM ==3)||(inNUM ==4)){
-                    switch(inNUM){
-                        case 0:
-                            System.out.println("The additive inverse of "+NUM+" is "+-NUM);
-                            break;
-                        case 1:
-                            System.out.println("The reciprocal of "+NUM+" is "+1/NUM);
-                            break;
-                        case 2:
-                            System.out.println(NUM+" Squared, is "+NUM*NUM);
-                            break;
-                        case 3:
-                            System.out.println(NUM+" Cubed, is "+NUM*NUM*NUM);
-                            break;
-                        case 4:
-                            System.out.println("Thank you, goodbye!");
-                    }
+            inNUM = sc.nextInt();
+            // Switching function according to the input
+            switch (inNUM) {
+                case 0 -> System.out.println("The additive inverse of " + NUM + " is " + -NUM);
+                case 1 -> System.out.println("The reciprocal of " + NUM + " is " + 1 / NUM);
+                case 2 -> System.out.println(NUM + " Squared, is " + NUM * NUM);
+                case 3 -> System.out.println(NUM + " Cubed, is " + NUM * NUM * NUM);
+                case 4 -> System.out.println("Thank you, goodbye!");
+                default -> {
+                    System.out.println("Invalid input, please try again!");
+                    inNUM = -2147483648;
                 }
-                else System.out.println("Invalid input, please try again!");
+            }
+        }
     }
 }
