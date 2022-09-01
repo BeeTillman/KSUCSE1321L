07cd8985aa96d9fe7f24718c1f8a640c98469f45
Bee Tillman <111484935+BeeTillman@users.noreply.github.com>
2022-09-01 12:23:14 -0400

    Added a new test Array, fixed Lab3 and Assignment2

--- src/Testing_Area/ArrayTest.java
+++ src/Testing_Area/ArrayTest.java
@@ -1,8 +1,26 @@
 package Testing_Area;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 // This class file contains an area where I can test the array function
 public class ArrayTest {
     public static void main(String[] args){
-        String[] myArr = {"Bee", "Rachel", "Michael", "Steve"};
-            System.out.println(myArr[1]);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter 5 numbers separated by pressing enter: ");
+            int firstIn = sc.nextInt();
+            int secondIn = sc.nextInt();
+            int thirdIn = sc.nextInt();
+            int fourthIn = sc.nextInt();
+            int fiveIn = sc.nextInt();
+                int[] myArr = {firstIn,secondIn,thirdIn,fourthIn,fiveIn};
+        int sum=0;
+            for (int value : myArr){
+                sum += value;
+            }
+            float avg = (float)sum/(float)myArr.length;
+                System.out.println("You entered: "+ Arrays.toString(myArr));
+                System.out.println("The sum of these numbers is "+sum);
+                System.out.println("The average of these numbers is "+avg);
 }
 }
