357c45b5de3cb507a474692d67e9c156fc5f42eb
BeeTillman <bpt3creations@gmail.com>
2022-09-01 22:14:43 -0400

    Testing Things

---
+++ src/Testing_Area/DiffChecker.java
@@ -0,0 +1,11 @@
+package Testing_Area;
+
+import java.util.Scanner;
+
+public class DiffChecker {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Copy and paste the expected output here: ");
+            String expectedOut = sc.next();
+    }
+}
