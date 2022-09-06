29ff00db0687f64ee4cd7f30482e3e60edfea980
Bee Tillman <111484935+BeeTillman@users.noreply.github.com>
2022-09-06 14:53:39 -0400

    Updated Lab6B

--- src/Module_6/Lab6B.java
+++ src/Module_6/Lab6B.java
@@ -12,6 +12,12 @@ import java.util.Random;
 
 public class Lab6B {
     public static void main(String[] args){
-
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+            System.out.println("Enter a number between 1 and 1000: ");
+                int inNUM = sc.nextInt();
+            while(inNUM<1000 && inNUM>1){
+                int guess =
+            }
     }
 }
