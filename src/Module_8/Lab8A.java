f69d8607cb5d0b0249d74baa7587ff055ab718b4
Bee Tillman <111484935+BeeTillman@users.noreply.github.com>
2022-09-08 14:47:44 -0400

    Added Lab8A

---
+++ src/Module_8/Lab8A.java
@@ -0,0 +1,20 @@
+package Module_8;
+
+import java.util.Scanner;
+
+/*
+Class:  CSE1321L
+Section:    J51
+Term:   Fall 2022
+Instructor: Jaskirat Singh Sohal
+Name:   Billups Tillman
+Lab/Assignment#:    8
+*/
+public class Lab8A {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter 5 integers for the first array:");
+        System.out.println("Integer 1: ");
+            int one = sc.nextInt();
+    }
+}
