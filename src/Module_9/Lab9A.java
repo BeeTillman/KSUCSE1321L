dc75fcd840b82b292f7cecc2bc6392b342c98b31
Bee Tillman <111484935+BeeTillman@users.noreply.github.com>
2022-09-15 14:52:04 -0400

    Completed Lab9A, added to Testing Area

---
+++ src/Module_9/Lab9A.java
@@ -0,0 +1,37 @@
+package Module_9;
+/*
+Class:  CSE1321L
+Section:    J51
+Term:   Fall 2022
+Instructor: Jaskirat Singh Sohal
+Name:   Billups Tillman
+Lab/Assignment#:    9
+*/
+import java.util.Scanner;
+
+public class Lab9A {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        // Declaring array and variables
+        int[] array = new int[11];
+        int target,j=1,bool=0;
+        // Collecting integer input and adding to the array
+        System.out.println("Please enter 10 numbers:");
+        for(int i=1;i<=10;i++){
+            System.out.println("Integer "+i+": ");
+                array[i]=sc.nextInt();
+        }
+        System.out.println("What is the target number: ");
+            target = sc.nextInt();
+        // Comparing target number to every number within the array
+        while(j<=10){
+            if(array[j]==target){
+                bool = 1;
+                j = 11;
+            }
+            j++;
+        }
+        if(bool==1) System.out.println("The target number is in the set.");
+        else System.out.println("The target number is not in the set.");
+    }
+}
---
+++ src/Testing_Area/PrePostFix.java
@@ -0,0 +1,14 @@
+package Testing_Area;
+
+public class PrePostFix {
+    public static void main(String[] args){
+        int x = 0;
+        System.out.println(x++);
+    }
+}
+
+
+/* NOTES */
+/* ++ after a variable ("x++") will add AFTER the variable is used
+IE, the above code will print "0", BUT if you were to loop it, the NEXT time, it would display "1"
+ */
