994a3b50f1099ac6566247464cdd7b88b2fdae0c
Bee Tillman <111484935+BeeTillman@users.noreply.github.com>
2022-09-20 15:06:56 -0400

    Adjusted Lab6B

--- src/Lab6B.java
+++ src/Lab6B.java
@@ -13,18 +13,18 @@ public class Lab6B {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
-        int guesses = 1;
-            System.out.println("Enter a number between 1 and 1000");
+        int guesses = 0;
+            System.out.println("Enter a number between 1 and 1000: ");
                 int inNUM = sc.nextInt();
             while(inNUM>1 && inNUM<1000){
                 int guess = rand.nextInt(1000);
+                guesses++;
                     if(guess==inNUM){
                         System.out.println("I guessed the number was "+guess+" and it only took me "+guesses+" guesses");
                         break;
                     }
                     else{
                         System.out.println("My guess was "+guess);
-                        guesses++;
                     }
             }
     }
