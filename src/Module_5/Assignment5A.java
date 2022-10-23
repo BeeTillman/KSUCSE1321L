602a3ee95f292d1e9b38c57fc8d7f9ca8172b71f
Bee Tillman <111484935+BeeTillman@users.noreply.github.com>
2022-10-23 19:15:07 -0400

    i love you
    i love you

---
+++ src/Module_5/Assignment5A.java
@@ -0,0 +1,72 @@
+package Module_5;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Assignment5A {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String noFriend = "";
+        String input, replacement;
+        String[] friends = {noFriend,noFriend,noFriend,noFriend,noFriend};
+        int choice,sent=0,friendNum=0,index;
+
+        System.out.println("[Top 5 Friend's List]");
+        while(sent!=1){
+            System.out.println("What would you like to do?");
+            System.out.println("1) Enter a friend's name");
+            System.out.println("2) Replace a friend's name");
+            System.out.println("3) Display your friends list");
+            System.out.println("4) Quit");
+                choice = sc.nextInt();
+            System.out.println("Your choice: "+choice);
+
+                switch(choice){
+                    case 1:
+                    System.out.println("Enter a name: ");
+                        input = sc.next();
+                    for(int i = 0; i < friends.length; i++) {
+                        if(friendNum==5){
+                            System.out.println("Sorry, your friends list is full!");
+                            break;
+                        }
+                        else if(friends[i].equalsIgnoreCase(input)){
+                            System.out.println("Sorry, they're already on the list!");
+                            break;
+                        }
+                        else if (friends[i].equals(noFriend)){
+                            friends[i]=input;
+                            friendNum++;
+                            break;
+                        }
+                    }
+                    break;
+
+                    case 2:
+                        System.out.println("Enter a name: ");
+                            input = sc.next();
+                        System.out.println("Enter an index: ");
+                            index = sc.nextInt();
+                            if(index<=5&&index>0){
+                                    System.out.println(input + " has replaced " + friends[index - 1] + " on your friends list!");
+                                    friends[index - 1] = input;
+                                    break;
+                            }
+                            else{
+                                System.out.println("Sorry, that's an invalid command!");
+                                break;
+                            }
+
+                    case 3:
+                    System.out.println("Friend's List:");
+                    for (int j = 0; j < 5; j++){
+                        System.out.println((j + 1) + ") " + friends[j]);
+                    }
+                    break;
+                    case 4:
+                        System.out.println("[Program ends]");
+                        sent = 1;
+                        break;
+                }
+            }
+        }
+}
--- src/Module_9/Lab9B.java
+++ src/Module_9/Lab9B.java
@@ -7,10 +7,56 @@ Instructor: Jaskirat Singh Sohal
 Name:   Billups Tillman
 Lab/Assignment#:    9
 */
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Lab9B {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[12];
+        int size = arr.length;
+
+        System.out.println("Please enter 11 numbers:");
+        for(int k = 0;k<11;k++){
+            System.out.println("Integer "+(k+1)+": ");
+            arr[k] = sc.nextInt();
+        }
+        for(int i = 0;i < size - 1;i++){
+            for(int j = 0;j < size-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        System.out.println("What is the target number: ");
+        int target = sc.nextInt();
+
+        System.out.println("The sorted set is: "+Arrays.toString(arr));
+
+        int low = 0, mid = 0;
+        int high = arr.length-1;
+        boolean found = false;
+        while(high>=low && found==false) {
+            mid = (low + high) / 2;
+            if (target < arr[mid]) {
+                high = mid - 1;
+            } else if (target == arr[mid]) {
+                found = true;
+            } else low = mid + 1;
+            if (found == false) {
+                System.out.println("Low is " + low);
+                System.out.println("High is " + high);
+                System.out.println("Mid is " + mid);
+                System.out.println("Searching");
+            }
+        }
+        if(found==true){
+            System.out.println("The target is in the set.");
+        }
+        else System.out.println("The target is not in the set.");
     }
 }
